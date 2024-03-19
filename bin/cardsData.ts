3b2c22244dd737f1758d5af25546b3e54110f22d
Sahil Alhat <sahilalhat@Sahils-MacBook-Pro.local>
2024-03-19 14:42:06 -0400

    Temp Card Data

---
+++ bin/cardsData.ts
@@ -0,0 +1,42 @@
+export const cardsData = [
+    {
+      id: 0,
+      title: "To Do",
+      components: [
+        {
+          id: 100,
+          name: "MATH 1226"
+        },
+        {
+          id: 200,
+          name: "CS 2114"
+        },
+      ]
+    },
+    {
+      id: 1,
+      title: "In Progress",
+      components: [
+        {
+          id: 300,
+          name: "PHYS 2206 "
+        },
+        {
+          id: 400,
+          name: "PHYS 2215"
+        },
+      ]
+    },
+    {
+      id: 2,
+      title: "Done",
+      components: [
+        {
+          id: 500,
+          name: "CS 2104"
+        },
+      ]
+    }
+  
+  
+  ]
