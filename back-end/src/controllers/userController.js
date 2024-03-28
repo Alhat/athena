ffd6f733fca71b4d40bd4ca9e0ca892b59bae5e6
joshmk31 <joshmk31@gmail.com>
2024-03-28 14:42:44 -0400

    added some more verbose warning messages if you type in a nested JSON (like canvas courses, settings or user stats) incorrectly

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,6 +1,81 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+/**
+ * 
+ * @param {*} courseArray 
+ * @returns a string (true) if has an error with type, else returns an empty string (false)
+ */
+function ensure_canvas_courses_type(courseArray) {
+
+    for (course of courseArray) {
+
+        if (course.course_id == undefined) {
+
+            return "Failure, no course_id found in one of the canvas courses provided!";
+
+        } else if (course.course_grade == undefined) {
+
+            return "Failure, no course_grade found in one of the canvas courses provided!";
+
+        } else if (course.passing_grade == undefined) {
+
+            return "Failure, no passing_grade found in one of the canvas courses provided!";
+
+        } else if (course.average_completion_time == undefined) {
+
+            return "Failure, no average_completion_time found in one of the canvas courses provided!";
+
+        }
+    }
+
+    return "";
+}
+
+/**
+ * 
+ * @param {*} settings 
+ * @returns a string (true) if has an error with type, else returns an empty string (false)
+ */
+function ensure_settings_type(settings) {
+    if (settings.notifications == undefined) {
+
+        return "Failure, no notifications found in your settings!";
+
+    } else if (settings.theme == undefined) {
+
+        return "Failure, no theme found in your settings!";
+
+    } else {
+
+        return "";
+    }
+}
+
+/**
+ * 
+ * @param {*} stats 
+ * @returns a string (true) if has an error with type, else returns an empty string (false)
+ */
+function ensure_user_stats_type(stats) {
+
+    if (stats.average_completion_time == undefined) {
+
+        return "Failure, no average_completion_time found in your user_stats!";
+
+    } else if (stats.average_grade == undefined) {
+
+        return "Failure, no average_grade found in your user_stats!";
+
+    } else if (stats.total_tasks_completed == undefined) {
+
+        return "Failure, no total_tasks_completed found in your user_stats!";
+
+    } else {
+
+        return "";
+    }
+}
 
 exports.setUser = async (req, res) => {
 
@@ -38,6 +113,21 @@ exports.setUser = async (req, res) => {
         if (!existingUserData) {
         
             // if (canvas_courses) console.log("canvas_courses is set to: \n", canvas_courses);
+
+            // Verify types of nested JSON objects
+            // Will be a string if verification fails, empty string if passes
+            if (canvas_courses) {
+                const canvas_courses_error = ensure_canvas_courses_type(canvas_courses);
+                if (canvas_courses_error) return res.send({ message: canvas_courses_error });
+            }
+            if (settings) {
+                const settings_error = ensure_settings_type(settings);
+                if (settings_error) return res.send({ message: settings_error });
+            }
+            if (user_stats) {
+                const user_stats_error = ensure_user_stats_type(user_stats);
+                if (user_stats_error) return res.send({ message: user_stats_error });
+            }
 
             // Set defaults if no info provided
             username = username || user_id;
@@ -100,6 +190,21 @@ exports.updateUser = async (req, res) => {
             return;
         }
 
+        // Verify types of nested JSON objects
+        // Will be a string if verification fails, empty string if passes
+        if (canvas_courses) {
+            const canvas_courses_error = ensure_canvas_courses_type(canvas_courses);
+            if (canvas_courses_error) return res.send({ message: canvas_courses_error });
+        }
+        if (settings) {
+            const settings_error = ensure_settings_type(settings);
+            if (settings_error) return res.send({ message: settings_error });
+        }
+        if (user_stats) {
+            const user_stats_error = ensure_user_stats_type(user_stats);
+            if (user_stats_error) return res.send({ message: user_stats_error });
+        }
+        
         await prisma.user_data.update({
             where: { id: user_id },
             data: {
