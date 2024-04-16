43c49a60f047058a127096b967375ef0c80d521a
Joel Llaury <joell@vt.edu>
2024-04-15 23:27:19 -0400

    Stopped generating tasks from tasks that already have been made into tasks.

--- back-end/src/services/gen_ass.py
+++ back-end/src/services/gen_ass.py
@@ -7,7 +7,7 @@ from dotenv import dotenv_values
 
 url = "http://localhost:5000/api/gpt"
 headers = {"Content-Type": "application/json"}
-users = ["josh_py"]
+users = ["giol"]
 config = dotenv_values("../../.env")
 
 def main():
